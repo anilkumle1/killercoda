28cdb107da1a11f2a3725bf9926742a6a7abd22f
Anil Kumle <anil.kumle@sakhaglobal.com>
2022-07-27 17:28:01 +0530

    adde

--- docker-copy/step1/verify.sh
+++ docker-copy/step1/verify.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cat application.py
+cat /root/application.py
 # docker exec -it docker stat /home/application.py
 # if docker exec -it docker stat /home/application.py; then exit 1; fi
 # if docker ps | grep "new-docker"; then exit 1; fi
