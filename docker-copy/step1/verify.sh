c09fa51f9448cba051c54f8f622f2e927b5be72c
anilkumle1 <95471130+anilkumle1@users.noreply.github.com>
2022-07-28 11:20:02 +0530

    Update verify.sh

--- docker-copy/step1/verify.sh
+++ docker-copy/step1/verify.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+docker exec -it docker stat ~/
 
-docker cp mycontainer:/home/application.py /root/filesystem/
-cat /filesystem/application.py
+# docker cp mycontainer:/home/application.py /root/filesystem/
+# cat /filesystem/application.py
 # docker exec -it docker stat /home/application.py
 # if docker exec -it docker stat /home/application.py; then exit 1; fi
 # if docker ps | grep "new-docker"; then exit 1; fi
